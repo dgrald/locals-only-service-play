be8f62cc7230cbbe24b0977b4db5d9be64aec5a7
dgrald <grald.dylan@gmail.com>
2016-12-31 12:30:12 -0700

    extend ElevationRetriever to work with polygons and lines

--- app/services/ElevationRetriever.scala
+++ app/services/ElevationRetriever.scala
@@ -2,6 +2,7 @@ package services
 
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import com.google.inject.Inject
+import play.api.libs.json.{Json, Reads, Writes}
 import play.api.libs.ws.WSClient
 
 import scala.concurrent.Future
@@ -12,15 +13,58 @@ import scala.concurrent.Future
 class ElevationRetriever(ws: WSClient, baseUrl: String) {
   @Inject def this(ws: WSClient) = this(ws, "https://maps.googleapis.com/maps/api/elevation")
 
-  def retrieveElevation(lat: Double, long: Double): Future[Double] = {
-    val locationQueryString = ("locations", s"$lat,$long")
+  implicit val elevationResultReads: Reads[ElevationResult] = ElevationResult.elevationResultReads
+
+  def retrieveElevation(location: Location): Future[Seq[ElevationPoint]] = {
+
+    def getLocationQueryString(points: Seq[(Double, Double)]): String = {
+      points.map {
+        case (lat, long) => s"$lat,$long"
+      }.mkString("|")
+    }
+
+    val locationQueryStringValue = location match {
+      case point: PointLocation => s"${point.lat},${point.long}"
+      case line: LineLocation => getLocationQueryString(line.points)
+      case polygon: PolygonLocation => getLocationQueryString(polygon.points)
+    }
+
+    val locationQueryString = ("locations", locationQueryStringValue)
     val keyQueryString = ("key", Constants.googleApiKey)
     ws.url(baseUrl + "/json").withQueryString(locationQueryString, keyQueryString).get().map { response =>
       val resultsJson = response.json
       val results = resultsJson \ "results"
-      val elevation = results(0) \ "elevation"
-      elevation.validate[Double].get
+      println(results)
+      val elevationResults = results.validate[List[ElevationResult]].get
+
+      def mapPointsToElevations(points: Seq[(Double, Double)]): Seq[ElevationPoint] = {
+        points.map {
+          case (lat, long) =>
+            val matchingPoint = elevationResults.find(r => r.location.lat == lat && r.location.lng == long)
+            ElevationPoint(lat, long, matchingPoint.get.elevation)
+        }
+      }
+
+      location match {
+        case point: PointLocation => List(ElevationPoint(point.lat, point.long, elevationResults.head.elevation))
+        case line: LineLocation => mapPointsToElevations(line.points)
+        case polygon: PolygonLocation => mapPointsToElevations(polygon.points)
+      }
     }
   }
 
+}
+
+case class ElevationPoint(lat: Double, lng: Double, elevation: Double)
+
+case class ElevationResult(elevation: Double, location: ElevationResultLocation, resolution: Double)
+
+case class ElevationResultLocation(lat: Double, lng: Double)
+
+object ElevationResult {
+  def elevationResultLocationReads: Reads[ElevationResultLocation] = Json.reads[ElevationResultLocation]
+
+  implicit val locationReads = elevationResultLocationReads
+
+  def elevationResultReads: Reads[ElevationResult] = Json.reads[ElevationResult]
 }
--- test/ElevationRetrieverSpec.scala
+++ test/ElevationRetrieverSpec.scala
@@ -4,7 +4,7 @@ import play.api.mvc._
 import play.api.routing.sird._
 import play.api.test._
 import play.core.server.Server
-import services.{Constants, ElevationRetriever}
+import services._
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -15,12 +15,12 @@ import scala.concurrent.duration._
 class ElevationRetrieverSpec extends PlaySpec {
 
   "ElevationRetriever.getElevation" should {
-    val lat = SomeRandom.double()
-    val long = SomeRandom.double()
 
-    val elevationResult = SomeRandom.double()
-
-    "call the google elevation API correctly" in {
+    "call the google elevation API correctly for a PointLocation" in {
+      val pointLocation = SomeRandom.pointLocation()
+      val elevationResult = SomeRandom.double()
+      val lat = pointLocation.lat
+      val long = pointLocation.long
       Server.withRouter() {
         case GET(p"/json") => Action { request =>
           val queries = request.queryString
@@ -28,17 +28,78 @@ class ElevationRetrieverSpec extends PlaySpec {
           val key = queries("key").head
           if(locations.equals(s"$lat,$long") && key.equals(Constants.googleApiKey)) {
             Results.Ok(Json.obj("results" ->
-              Json.arr(Json.obj("elevation" -> elevationResult, "location" -> Json.obj("lat" -> lat, "lng" -> long))),
-              "resolution" -> SomeRandom.double()))
+              Json.arr(Json.obj("elevation" -> elevationResult, "location" -> Json.obj("lat" -> lat, "lng" -> long), "resolution" -> SomeRandom.double()))))
           } else {
             Results.BadRequest("")
           }
         }
       } { implicit port =>
         WsTestClient.withClient { client =>
-          val result = Await.result(new ElevationRetriever(client, "").retrieveElevation(lat, long), 10.seconds)
+          val result = Await.result(new ElevationRetriever(client, "").retrieveElevation(pointLocation), 10.seconds)
 
-          result mustEqual elevationResult
+          val expected = List(ElevationPoint(pointLocation.lat, pointLocation.long, elevationResult))
+          result mustEqual expected
+        }
+      }
+    }
+
+    "call the google elevation API correctly for a LineLocation" in {
+      val lineLocation = SomeRandom.lineLocation()
+      testLocation(lineLocation)
+    }
+
+    "call the google elevation API correctly for a PolygonLocation" in {
+      val polygonLocation = SomeRandom.polygonLocation()
+      testLocation(polygonLocation)
+    }
+
+    def testLocation(location: Location) = {
+      def getElevationPoints(points: Seq[(Double, Double)]): Array[ElevationPoint] = {
+        points.map(p => ElevationPoint(p._1, p._2, SomeRandom.double())).toArray
+      }
+
+      def getPointsParam(points: Seq[(Double, Double)]): String = {
+        points.map {
+          case (lat, lng) => s"$lat,$lng"
+        }.mkString("|")
+      }
+
+      def getElevationResult(points: Seq[(Double, Double)], elevationResults: Array[ElevationPoint]): Seq[JsObject] = {
+        points.zipWithIndex.map {
+          case ((lat, lng), index) => Json.obj("elevation" -> elevationResults(index).elevation, "location" -> Json.obj("lat" -> lat, "lng" -> lng), "resolution" -> SomeRandom.double())
+        }
+      }
+
+      val elevationResults = location match {
+        case l: LineLocation => getElevationPoints(l.points)
+        case p: PolygonLocation => getElevationPoints(p.points)
+      }
+      val locationsInput = location match {
+        case l: LineLocation => getPointsParam(l.points)
+        case p: PolygonLocation => getPointsParam(p.points)
+      }
+
+      val results = location match {
+        case l: LineLocation => getElevationResult(l.points, elevationResults)
+        case p: PolygonLocation => getElevationResult(p.points, elevationResults)
+      }
+
+      Server.withRouter() {
+        case GET(p"/json") => Action { request =>
+          val queries = request.queryString
+          val locations = queries("locations").head
+          val key = queries("key").head
+          if(locations.equals(locationsInput) && key.equals(Constants.googleApiKey)) {
+            Results.Ok(Json.obj("results" ->  results))
+          } else {
+            Results.BadRequest("")
+          }
+        }
+      } { implicit port =>
+        WsTestClient.withClient { client =>
+          val result = Await.result(new ElevationRetriever(client, "").retrieveElevation(location), 10.seconds)
+
+          result mustEqual elevationResults
         }
       }
     }
